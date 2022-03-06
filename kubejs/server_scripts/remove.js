daf82829ef77924bc36f507fa6ddf65e2a26b404
Alexandre BREAULT <a.breault@iia-laval.fr>
2022-03-06 02:14:57 +0100

    add function for remove craft

--- kubejs/server_scripts/remove.js
+++ kubejs/server_scripts/remove.js
@@ -1,19 +1,32 @@
 onEvent('recipes', event => {
-    event.remove({ id: 'tconstruct:common/flint'})
-    event.remove({ id: 'mysticalagriculture:essence/flint' })
-    event.remove({ type: 'exnihilosequentia:sieve' })
-    event.remove({ id: 'mekanism:processing/lead/ingot/from_nuggets'})
-    event.remove({ id: 'thermal:storage/lead_ingot_from_nuggets'})
-    event.remove({ id: 'thermal:storage/lead_ingot_from_block'})
-    event.remove({ id: 'immersiveengineering:crafting/nugget_lead_to_ingot_lead'})
-    event.remove({ id: 'immersiveengineering:crafting/storage_lead_to_ingot_lead'})
-    event.remove({ id: 'immersiveengineering:crafting/nugget_copper_to_ingot_copper'})
-    event.remove({ id: 'mekanism:processing/copper/ingot/from_nuggets'})
-    event.remove({ id: 'thermal:storage/copper_ingot_from_nuggets'})
-    event.remove({ id: 'tconstruct:common/materials/copper_ingot_from_nuggets'})
-    event.remove({ id: 'tconstruct:common/materials/copper_ingot_from_block'})
-    event.remove({ id: 'immersiveengineering:crafting/storage_copper_to_ingot_copper'})
-    event.remove({ id: 'create:crafting/materials/copper_ingot_from_decompacting' })
-    event.remove({ id: 'mekanism:processing/copper/ingot/from_block'})
+    const removeCraft = (element, key) => {
+        if (key == 'id') {
+            event.remove({id: element})
+        } else if (key == 'type') {
+            event.remove({ type: element})
+        }
+    }
+    let data = [
+        { id: 'tconstruct:common/flint' },
+        { id: 'mysticalagriculture:essence/flint' },
+        { type: 'exnihilosequentia:sieve' },
+        { id: 'thermal:storage/lead_ingot_from_nuggets'},
+        { id: 'thermal:storage/lead_ingot_from_block'},
+        { id: 'immersiveengineering:crafting/nugget_lead_to_ingot_lead'},
+        { id: 'immersiveengineering:crafting/storage_lead_to_ingot_lead'},
+        { id: 'immersiveengineering:crafting/nugget_copper_to_ingot_copper'},
+        { id: 'mekanism:processing/copper/ingot/from_nuggets'},
+        { id: 'thermal:storage/copper_ingot_from_nuggets'},
+        { id: 'tconstruct:common/materials/copper_ingot_from_nuggets'},
+        { id: 'tconstruct:common/materials/copper_ingot_from_block'},
+        { id: 'immersiveengineering:crafting/storage_copper_to_ingot_copper'},
+        { id: 'create:crafting/materials/copper_ingot_from_decompacting' },
+        { id: 'mekanism:processing/copper/ingot/from_block'},
+    ]
     
+    for (var i = 0; i < data.length; i++){
+        for (var key in data[i]) {
+          removeCraft(data[i][key], key)
+      }
+    }    
 })
