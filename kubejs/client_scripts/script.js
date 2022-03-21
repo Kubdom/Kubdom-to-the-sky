bcfb248d34dfa3d8ec6319414e8ad6cb5fac0f9b
Alexandre BREAULT <a.breault@iia-laval.fr>
2022-03-21 17:48:50 +0100

    add hide jei item

--- kubejs/client_scripts/script.js
+++ kubejs/client_scripts/script.js
@@ -2,14 +2,22 @@
 
 console.info('Hello, World! (You will see this line every time client resources reload)')
 
+console.info('Hello, World! (You will see this line every time client resources reload)')
+
 onEvent('jei.hide.items', event => {
 	event.hide([
-		/appliedenergistics2:facade/
+		/appliedenergistics2:facade/,
+		/tconstruct:crafting_station/,
+		/tconstruct:tinker_station/,
+		/tconstruct:part_builder/,
 	])
 })
 
 onEvent('jei.add.items', event => {
 	event.add([
-		Item.of('appliedenergistics2:facade', '{item:"minecraft:stone"}')
+		Item.of('appliedenergistics2:facade', '{item:"minecraft:stone"}'),
+		Item.of('tconstruct:crafting_station', '{texture:"minecraft:oak_log"}'),
+		Item.of('tconstruct:tinker_station', '{texture:"minecraft:oak_planks"}'),
+		Item.of('tconstruct:part_builder', '{texture:"minecraft:oak_planks"}')
 	])
   })
