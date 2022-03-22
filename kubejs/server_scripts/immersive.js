e216548d0139edff442a4c803aab154d3a30a430
Aoxis <sourdin.killian@gmail.com>
2022-03-22 22:11:45 +0100

    add oil fluid add craft oil fluid change craft graphite dust in squeezer

---
+++ kubejs/server_scripts/immersive.js
@@ -0,0 +1,43 @@
+onEvent('recipes', event => {
+
+    event.custom({
+      "type":"immersiveengineering:refinery",
+      "result":{"fluid":"kubejs:oil","amount":16},
+      "input0":{"fluid":"immersiveengineering:biodiesel","amount":8},
+      "input1":{"fluid":"immersivepetroleum:oil","amount":8},
+      "energy":80
+      })
+      
+    event.custom({
+      "type":"immersiveengineering:squeezer",
+      "result":{"tag":"forge:dusts/hop_graphite"},
+      "fuild":{"fluid":"immersivepetroleum:oil"},
+      "input":{"count":8,"base_ingredient":{"tag":"forge:dusts/coal_coke"}},
+      "energy":19200
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+})
---
+++ kubejs/startup_scripts/fluids.js
@@ -0,0 +1,5 @@
+onEvent('fluid.registry', event => {
+
+    // Create Oil liquid
+    event.create('oil').textureThin(0x000000).bucketColor(0x000000).displayName('Oil Liquid')
+  })
