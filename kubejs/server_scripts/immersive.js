d42c4e4b9dda95e777ca5c518a3d8bafda938c58
Alexandre BREAULT <a.breault@iia-laval.fr>
2022-03-23 10:52:47 +0100

    add craft custom fluid

--- kubejs/server_scripts/immersive.js
+++ kubejs/server_scripts/immersive.js
@@ -1,20 +1,47 @@
 onEvent('recipes', event => {
 
-    event.custom({
-      "type":"immersiveengineering:refinery",
-      "result":{"fluid":"kubejs:oil","amount":16},
-      "input0":{"fluid":"immersiveengineering:biodiesel","amount":8},
-      "input1":{"fluid":"immersivepetroleum:oil","amount":8},
-      "energy":80
-      })
-      
-    event.custom({
-      "type":"immersiveengineering:squeezer",
-      "result":{"tag":"forge:dusts/hop_graphite"},
-      "fuild":{"fluid":"immersivepetroleum:oil"},
-      "input":{"count":8,"base_ingredient":{"tag":"forge:dusts/coal_coke"}},
-      "energy":19200
-    })
+  // event.custom({
+  //   "type":"immersiveengineering:refinery",
+  //   "result":{"fluid":"kubejs:oil","amount":16},
+  //   "input0":{"fluid":"immersiveengineering:biodiesel","amount":8},
+  //   "input1":{"fluid":"immersivepetroleum:oil","amount":8},
+  //   "energy":80
+  //   })
+
+  event.custom({
+    "type": "immersiveengineering:refinery",
+    "result": {
+      "fluid": "kubejs:oil",
+      "amount": 16
+    },
+    "input0": {
+      "tag": "forge:biodiesel",
+      "amount": 8
+    },
+    "input1": {
+      "tag": "forge:ethanol",
+      "amount": 8
+    },
+    "energy": 80
+  })
+
+
+  event.custom({
+    "type": "immersiveengineering:squeezer",
+    "result": {
+      "tag": "forge:dusts/hop_graphite"
+    },
+    "fuild": {
+      "fluid": "immersivepetroleum:oil"
+    },
+    "input": {
+      "count": 8,
+      "base_ingredient": {
+        "tag": "forge:dusts/coal_coke"
+      }
+    },
+    "energy": 19200
+  })
 
 
 
@@ -39,5 +66,5 @@ onEvent('recipes', event => {
 
 
 
-	
+
 })
