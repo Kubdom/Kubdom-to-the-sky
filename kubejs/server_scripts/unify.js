612f541e0beb3cf52d6b074c6b6227a9f6a062a5
Aoxis <sourdin.killian@gmail.com>
2022-03-28 23:05:32 +0200

    add minerais tin in unify

--- kubejs/server_scripts/unify.js
+++ kubejs/server_scripts/unify.js
@@ -7,12 +7,13 @@ onEvent('recipes', (event) => {
     // nuggets
     let copperNugget = ['#forge:nuggets/copper', 'mekanism:nugget_copper', 'thermal:copper_nugget','tconstruct:copper_nugget','immersiveengineering:nugget_copper','create:copper_nugget']
     let leadNugget = ['#forge:nuggets/lead', 'eidolon:lead_nugget', 'mekanism:nugget_lead','thermal:lead_nugget','immersiveengineering:nugget_lead']
+    let tinNugget = ['#forge:nuggets/tin', 'mekanism:nugget_tin','thermal:tin_nugget']
 
 
     // ingots
     let copperIngot = ['#forge:ingots/copper', 'mekanism:ingot_copper', 'immersiveengineering:ingot_copper', 'projectred-core:copper_ingot', 'create:copper_ingot', 'thermal:copper_ingot', 'tconstruct:copper_ingot']
     let leadIngot = ['#forge:ingots/lead', 'mekanism:ingot_lead', 'eidolon:lead_ingot', 'mekanism:ingot_lead', 'thermal:lead_ingot', 'immersiveengineering:ingot_lead']
-
+    let tinIngot = ['#forge:ingots/tin', 'mekanism:ingot_tin','thermal:tin_ingot', 'projectred-core:tin_ingot', 'exnihilosequentia:ingot_tin']
 
     // blocks
     let copperBlock = ['#forge:storage_blocks/copper', 'create:copper_block', 'immersiveengineering:storage_copper', 'mekanism:block_copper', 'tconstruct:copper_block', 'thermal:copper_block']
@@ -25,9 +26,11 @@ onEvent('recipes', (event) => {
 
     copperIngot.forEach(input => { replaceIO(input, 'thermal:copper_ingot') })
     leadIngot.forEach(input => { replaceIO(input, 'eidolon:lead_ingot') })
+    tinIngot.forEach(input => { replaceIO(input, 'thermal:tin_ingot') })
 
     copperNugget.forEach(input => { replaceIO(input, 'thermal:copper_nugget') })
     leadNugget.forEach(input => { replaceIO(input, 'eidolon:lead_nugget') })
+    tinNugget.forEach(input => { replaceIO(input, 'eidolon:lead_nugget') })
 
     copperBlock.forEach(input => { replaceIO(input, 'thermal:copper_block') })
     leadBlock.forEach(input => { replaceIO(input, 'eidolon:lead_block') })
